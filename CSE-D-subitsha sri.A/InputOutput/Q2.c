8e8f51ca60d683b1398b74b834bb59b91b58b943
Subitshasri <asubitshasri@gmail.com>
2025-08-29 12:03:01 +0530

    Add files via upload

---
+++ CSE-D-subitsha sri.A/InputOutput/Q2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	// "book" -> string -> set of char ->'b' 'o' 'o' 'k'
+	char str[20];// vasanth priyan
+	scanf("%s",str); // which will take the string upto whitespace
+	printf("%s",str);
+}
