0e9cd04649e342478ba86fcff65cd4ad1bb5b996
Subitshasri <asubitshasri@gmail.com>
2025-08-30 10:29:38 +0530

    Add files via upload

---
+++ CSE-D-subitsha sri.A/InputOutput/Q10.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+ int num;
+ printf("enter octal number:");
+ scanf("%o",&num);
+printf("the number in integer:%d\n",num);
+return 0;
+}
---
+++ CSE-D-subitsha sri.A/InputOutput/Q9.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+	int num;
+	scanf("%x,&num");
+	printf("the number in integer format is:%d",num);
+	return 0;
+}
